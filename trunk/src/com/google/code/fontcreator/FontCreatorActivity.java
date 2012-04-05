9066da7a595f698a39a7641354f95e303c8e13e4
ch33zer@gmail.com <ch33zer@gmail.com@5b11e892-fbb6-37a9-b9d3-17f8315adb30>
2012-04-05 21:00:31 +0000

    Initial Commit

---
+++ trunk/src/com/google/code/fontcreator/FontCreatorActivity.java
@@ -0,0 +1,13 @@
+package com.google.code.fontcreator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FontCreatorActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
