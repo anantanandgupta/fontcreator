1a26f23787547db04b8933f536d2210d5cb141f2
Bonbon56 <Bonbon56@gmail.com>
2012-04-09 20:09:46 +0000

    Help xml and activity.

---
+++ src/com/google/code/fontcreator/HelpActivity.java
@@ -0,0 +1,12 @@
+package com.google.code.fontcreator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelpActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.help);
+	}
+}
