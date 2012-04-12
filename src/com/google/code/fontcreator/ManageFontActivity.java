b4cf7ef162fa49481487c3dda467e0e164067f08
Bonbon56 <Bonbon56@gmail.com>
2012-04-12 19:51:45 +0000

    1. Made select font dialog xml into Relative Layout. 2. Made transitions for Main Menu: 	-New Font 	-Edit Font 	-Help 3. Made transitions for Manage Font 	-Edit 	-Display 	-Use 	(Export couldn't be done because there is no export activity.)

--- src/com/google/code/fontcreator/ManageFontActivity.java
+++ src/com/google/code/fontcreator/ManageFontActivity.java
@@ -1,12 +1,56 @@
 package com.google.code.fontcreator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class ManageFontActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.managefont);
+		
+		final Button editButton = (Button)findViewById(R.id.edit_button);
+		editButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent myIntent = new Intent(v.getContext(), DrawActivity.class);
+                startActivityForResult(myIntent, 0);				
+			}
+		});
+		
+		final Button exportButton = (Button)findViewById(R.id.export_button);
+		exportButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent myIntent = new Intent(v.getContext(), ManageFontActivity.class);
+                startActivityForResult(myIntent, 0);				
+			}
+		});
+		
+		final Button displayButton = (Button)findViewById(R.id.display_button);
+		displayButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent myIntent = new Intent(v.getContext(), DisplayActivity.class);
+                startActivityForResult(myIntent, 0);				
+			}
+		});
+		
+		final Button useButton = (Button)findViewById(R.id.use_button);
+		useButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent myIntent = new Intent(v.getContext(), UseActivity.class);
+                startActivityForResult(myIntent, 0);				
+			}
+		});
 	}
 }
