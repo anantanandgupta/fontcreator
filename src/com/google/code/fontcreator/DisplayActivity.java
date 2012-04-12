03c5b6b4a547179ed5b1f75b7da19e5df9f83fbf
sharonlixx <sharonlixx@gmail.com>
2012-04-12 19:58:51 +0000

    added a class for sendActivity, also, now there are working buttons for the use and display activities

--- src/com/google/code/fontcreator/DisplayActivity.java
+++ src/com/google/code/fontcreator/DisplayActivity.java
@@ -1,22 +1,43 @@
 package com.google.code.fontcreator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DisplayActivity extends Activity{
 	private TextView fontDisplayTextView;
+	private Button mainMenuButton;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.displayfont);
 		initFontDisplay();
+		initButtons();
 	}
 	
 	private void initFontDisplay(){
 		fontDisplayTextView = (TextView) findViewById(R.id.fontDisplayTextView);
 		fontDisplayTextView.setText(getString(R.string.UCLetters)
 				+"\n" +getString(R.string.LCLetters)+ "\n" + getString(R.string.symbols));
+	}
+	
+	private void initButtons() {
+		// create the mainMenuButton
+		mainMenuButton = (Button) findViewById(R.id.menuButton);
+
+		// listener for the button
+		mainMenuButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				Intent intent = new Intent(DisplayActivity.this, MainMenuActivity.class);
+	        	startActivity(intent);
+			}
+
+		});
+
+		
 	}
 }
--- src/com/google/code/fontcreator/UseActivity.java
+++ src/com/google/code/fontcreator/UseActivity.java
@@ -1,8 +1,12 @@
 package com.google.code.fontcreator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 
@@ -10,13 +14,23 @@ public class UseActivity extends Activity {
 	
 	private Spinner fontSpinner;
 	
+	private EditText noteEditText;
+
+	private Button sendButton;
+	private Button clearButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.usefont);
 		initFontSpinner();
+		initEditText();
+		initButtons();
 	}
 	
+	/**
+	 * Initialize the Font Spinner to selct font.
+	 */
 	private void initFontSpinner(){
 		fontSpinner = (Spinner) findViewById(R.id.chooseFontSpinner);
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
@@ -24,5 +38,36 @@ public class UseActivity extends Activity {
 				android.R.layout.simple_spinner_item);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		fontSpinner.setAdapter(adapter);
+	}
+	/**
+	 * Initialize the EditText that lets user write the note
+	 */
+	private void initEditText(){
+		noteEditText = (EditText) findViewById(R.id.noteEditText);
+	}
+	
+	private void initButtons() {
+		// create the buttons
+		sendButton = (Button) findViewById(R.id.sendButton);
+		clearButton = (Button) findViewById(R.id.clearButton);
+
+		// listeners for the buttons
+		sendButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				Intent intent = new Intent(UseActivity.this, SendActivity.class);
+	        	startActivity(intent);
+			}
+
+		});
+		
+		clearButton.setOnClickListener(new View.OnClickListener() {
+			//clears the edit text
+			public void onClick(View view) {
+				noteEditText.setText("");
+			}
+
+		});
+
+		
 	}
 }
