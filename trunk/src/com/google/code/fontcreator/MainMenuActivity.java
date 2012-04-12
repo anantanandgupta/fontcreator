820864a014d1b411441b913bb94919fd54d10317
Bonbon56@gmail.com <Bonbon56@gmail.com@5b11e892-fbb6-37a9-b9d3-17f8315adb30>
2012-04-12 19:51:45 +0000

    1. Made select font dialog xml into Relative Layout. 2. Made transitions for Main Menu: 	-New Font 	-Edit Font 	-Help 3. Made transitions for Manage Font 	-Edit 	-Display 	-Use 	(Export couldn't be done because there is no export activity.)

--- trunk/src/com/google/code/fontcreator/MainMenuActivity.java
+++ trunk/src/com/google/code/fontcreator/MainMenuActivity.java
@@ -1,8 +1,9 @@
 package com.google.code.fontcreator;
 
 import android.app.Activity;
-import android.app.Dialog;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,15 +19,58 @@ public class MainMenuActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mainmenu);
-
+		final Button newFontButton = (Button)findViewById(R.id.new_font_button);
+		newFontButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent myIntent = new Intent(v.getContext(), DrawActivity.class);
+                startActivityForResult(myIntent, 0);
+			}
+		});
 		final Button existingFontButton = (Button)findViewById(R.id.existing_font_button);
 		existingFontButton.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				Dialog viewDialog = new Dialog(v.getContext()); 
+				final AlertDialog viewDialog;
+				AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+			
+				LayoutInflater li = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE); 
+				View dialogView = li.inflate(R.layout.existingfontdialogue, null); 
+				
+				builder.setView(dialogView);
+				viewDialog = builder.create();
+				
+				Spinner spinner = (Spinner) dialogView.findViewById(R.id.chooseExistingFontSpinner); 
+				ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(v.getContext(), R.array.fonts, android.R.layout.simple_spinner_item); 
+				adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); 
+				spinner.setAdapter(adapter); 
+				
+				final Button okButton = (Button)dialogView.findViewById(R.id.ok_button);
+				okButton.setOnClickListener(new OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						Intent myIntent = new Intent(v.getContext(), ManageFontActivity.class);
+		                startActivityForResult(myIntent, 0);	
+		                //Will put code for specific font?
+					}
+				});
+				final Button cancelButton = (Button)dialogView.findViewById(R.id.cancel_button);
+				cancelButton.setOnClickListener(new OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						viewDialog.cancel();
+					}
+				});
+				
+				viewDialog.show();
+				
+				
+				/*final Dialog viewDialog = new Dialog(v.getContext()); 
 				viewDialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND, WindowManager.LayoutParams.FLAG_BLUR_BEHIND); 
-				viewDialog.setTitle("Select Font"); 
 
 				LayoutInflater li = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE); 
 				View dialogView = li.inflate(R.layout.existingfontdialogue, null); 
@@ -36,6 +80,34 @@ public class MainMenuActivity extends Activity{
 				ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(v.getContext(), R.array.fonts, android.R.layout.simple_spinner_item); 
 				adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); 
 				spinner.setAdapter(adapter); 
+				
+				final Button okButton = (Button)dialogView.findViewById(R.id.ok_button);
+				okButton.setOnClickListener(new OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						Intent myIntent = new Intent(v.getContext(), ManageFontActivity.class);
+		                startActivityForResult(myIntent, 0);	
+		                //Will put code for specific font?
+					}
+				});
+				final Button cancelButton = (Button)dialogView.findViewById(R.id.cancel_button);
+				cancelButton.setOnClickListener(new OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						viewDialog.cancel();
+					}
+				});*/
+			}
+		});
+		final Button helpButton = (Button)findViewById(R.id.help_button);
+		helpButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent myIntent = new Intent(v.getContext(), HelpActivity.class);
+                startActivityForResult(myIntent, 0);
 			}
 		});
 	}
