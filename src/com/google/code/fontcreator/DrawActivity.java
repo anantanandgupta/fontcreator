3da931c45a50223946222813ebea6e3d7645fd8c
Bonbon56 <Bonbon56@gmail.com>
2012-05-02 01:02:42 +0000

    Implemented next/prev buttons and select letter button in DrawActivity.

--- src/com/google/code/fontcreator/DrawActivity.java
+++ src/com/google/code/fontcreator/DrawActivity.java
@@ -1,25 +1,40 @@
 package com.google.code.fontcreator;
 
+import com.google.typography.font.sfntly.table.truetype.Glyph;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.PopupWindow;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class DrawActivity extends Activity implements OnClickListener {
 
 	private ImageButton straightLineToolButton, freeDrawToolButton,
-			curvedLineToolButton, clearToolButton, 
-			undoButton, redoButton;
+	curvedLineToolButton, clearToolButton, 
+	undoButton, redoButton;
 	private Button currentLetterDisplayButton, prevButton, saveButton,
-			nextButton;
+	nextButton;
 
 	private DrawPanel drawPanel;
+
+	//private FontManager fontManager;
+
+	AlphabetIterator ai;
 
 	public enum DrawingTools {
 		straightLine, freeDraw, curvedLine
@@ -54,6 +69,8 @@ public class DrawActivity extends Activity implements OnClickListener {
 		nextButton.setOnClickListener(this);
 		currentTool = DrawingTools.straightLine;
 		drawPanel.setCurrentTool(DrawingTools.straightLine);
+		//fontManager = new FontManager();
+		ai = new AlphabetIterator();
 		updateToolHighlight();
 	}
 
@@ -114,12 +131,46 @@ public class DrawActivity extends Activity implements OnClickListener {
 			drawPanel.redo();
 			break;
 		case R.id.currentLetterDisplayButton:
+			final AlertDialog viewDialog;
+			AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+
+			LayoutInflater li = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE); 
+			View dialogView = li.inflate(R.layout.letter_selector_popup, null); 
+
+			builder.setView(dialogView);
+			viewDialog = builder.create();
+
+			final EditText letterselect = (EditText)dialogView.findViewById(R.id.select_letter_edit);
+			Button selectButton = (Button)dialogView.findViewById(R.id.select_letter_button);
+			selectButton.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View view) {
+					String abc = letterselect.getText().toString();
+					currentLetterDisplayButton.setText(abc);
+					updateGlyph(abc);
+					ai.setCurrent(abc);
+					viewDialog.cancel();
+				}
+			});
+			Button cancelButton = (Button)dialogView.findViewById(R.id.cancel_select_letter_button);
+			cancelButton.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View view) {
+					viewDialog.cancel();
+				}
+			});
+			viewDialog.show();
 			break;
 		case R.id.prevButton:
+				updateGlyph(ai.prev());
 			break;
 		case R.id.saveButton:
+			saveGlyphDialog();
 			break;
 		case R.id.nextButton:
+			updateGlyph(ai.next());
 			break;
 		}
 	}
@@ -131,16 +182,65 @@ public class DrawActivity extends Activity implements OnClickListener {
 		switch (currentTool) {
 		case straightLine:
 			straightLineToolButton
-					.setBackgroundResource(R.color.transparentbuttonselected);
+			.setBackgroundResource(R.color.transparentbuttonselected);
 			break;
 		case freeDraw:
 			freeDrawToolButton
-					.setBackgroundResource(R.color.transparentbuttonselected);
+			.setBackgroundResource(R.color.transparentbuttonselected);
 			break;
 		case curvedLine:
 			curvedLineToolButton
-					.setBackgroundResource(R.color.transparentbuttonselected);
+			.setBackgroundResource(R.color.transparentbuttonselected);
 			break;
 		}
+	}
+
+	private void saveGlyphDialog(){
+		final AlertDialog viewDialog;
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+		LayoutInflater li = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE); 
+		View dialogView = li.inflate(R.layout.save_dialog, null); 
+
+		builder.setView(dialogView);
+		viewDialog = builder.create();
+
+		Button saveButton = (Button)dialogView.findViewById(R.id.save_glyph_button);
+		saveButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				if(saveGlyph())
+				{
+					Toast toast = Toast.makeText(getApplicationContext(), "Letter saved!", Toast.LENGTH_LONG);
+					toast.show();
+				}
+				else
+				{
+					Toast toast = Toast.makeText(getApplicationContext(), "Error saving letter, no save made.", Toast.LENGTH_LONG);
+					toast.show();
+				}
+				viewDialog.cancel();
+			}
+		});
+		Button cancelButton = (Button)dialogView.findViewById(R.id.cancel_save_glyph_button);
+		cancelButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				viewDialog.cancel();
+			}
+		});
+		viewDialog.show();
+	}
+
+	private boolean saveGlyph() {
+		return true;
+	}
+	private void updateGlyph(String glyphCharacter) {
+		saveGlyphDialog();
+		currentLetterDisplayButton.setText(ai.getCurrent());
+		//Glyph g = fontManager.getGlyph(glyphCharacter);
+		//TODO: Set drawing table to glyph
 	}
 }
